6617ddb42ceb171fc6dac062998556b8e8e7efd4
EricksonRT <erik_lauri@hotmail.com>
2022-04-08 20:21:33 -0300

    primeros componentes

--- js/main.js
+++ js/main.js
@@ -1,65 +1,16 @@
-const demo = (a, b, c, d, e) => {
-    let contMax = 0;
-    let contMin = 0;
-    n1 = a;
-    n2 = b;
-    n3 = c;
-    n4 = d;
-    n5 = e;
-    // Determina mayor
-    if ((n1 > n2) && (n1 > n3) && (n1 > n4) && (n1 > n5)) {
-        contMax = n1;
-    }
-    if ((n2 > n1) && (n2 > n3) && (n2 > n4) && (n2 > n5)) {
-        contMax = n2;
-    }
-    if ((n3 > n1) && (n3 > n3) && (n3 > n4) && (n3 > n5)) {
-        contMax = n3;
-    }
-    if ((n4 > n1) && (n4 > n3) && (n4 > n4) && (n4 > n5)) {
-        contMax = n4;
-    }
-    if ((n5 > n1) && (n5 > n3) && (n5 > n4) && (n5 > n2)) {
-        contMax = n5;
+//Cargamos los datos con la url
+let url = "https://www.omdbapi.com/?i=tt3896198&apikey=669a7ccb&";
+fetch(url)
+    .then(response => response.json())
+    .then(data => cargaBienvenida(data))
+    .catch(error => console.log(error))
+
+//Se pasa el valor de data la funcion
+const cargaBienvenida = (data) => {
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        const element = data[i];
+        // console.log("Elemt: " + element.Actors);
     }
 
-    console.log("Maximo: " + contMax);
-    console.log("Minimo: " + contMin);
 }
-
-
-const demo2 = () => {
-    let contMax = 0;
-    let contMin = 0;
-    datoInicial = prompt("meté un numero");
-    contMax = -19999;
-    contMin = 999999999999999999999;
-    console.log(datoInicial);
-    for (let i = 0; i < 6; i++) {
-        datoDos = prompt("meté un numero");
-        if (datoDos > datoInicial) {
-            contMax = datoDos;
-
-        } else if (datoDos < datoInicial) {
-            contMax = datoInicial;
-
-        }
-        console.log(datoDos);
-    }
-
-    console.log("Maximo: " + contMax);
-    console.log("Minimo: " + contMin);
-}
-
-
-// else if ((n1 < n2) && (n1 < n3) && (n1 < n4) && (n1 < n5)) {
-//     contMin = n1;
-// } else if ((n2 < n1) && (n2 < n3) && (n2 < n4) && (n2 < n5)) {
-//     contMin = n2;
-// } else if ((n3 < n1) && (n3 < n3) && (n3 < n4) && (n3 < n5)) {
-//     contMin = n3;
-// } else if ((n4 < n1) && (n4 < n3) && (n4 < n4) && (n4 < n5)) {
-//     contMin = n4;
-// } else if ((n5 < n1) && (n5 < n3) && (n5 < n4) && (n5 < n5)) {
-//     contMin = n5;
-// }
