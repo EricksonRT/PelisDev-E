8ef944592072596189fef62a26b6b3757d358737
EricksonRT <erik_lauri@hotmail.com>
2022-04-09 11:51:31 -0300

    primeros pasos

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-//Cargamos los datos con la url
-let url = "https://www.omdbapi.com/?i=tt3896198&apikey=669a7ccb&";
-fetch(url)
-    .then(response => response.json())
-    .then(data => cargaBienvenida(data))
-    .catch(error => console.log(error))
-
 //Se pasa el valor de data la funcion
 const cargaBienvenida = (data) => {
-    console.log(data);
-    for (let i = 0; i < data.length; i++) {
-        const element = data[i];
-        // console.log("Elemt: " + element.Actors);
-    }
-
+    alert(data);
 }
+const btnBuscar = document.getElementById("btn_buscar");
+btnBuscar.addEventListener("click", () => {
+    let titulo = document.getElementById("titulo").value;
+    //Cargamos los datos con la url
+    let url = "https://www.omdbapi.com/?i=tt3896198&apikey=669a7ccb&t=" + titulo;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => cargaBienvenida(data))
+        .catch(error => console.log(error))
+
+})
