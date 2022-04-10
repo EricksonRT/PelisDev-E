9deb1d1fe69733d9b9e2464b39964131795511fa
EricksonRT <erik_lauri@hotmail.com>
2022-04-10 18:38:17 -0300

    primeras medias querys

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 // Generador de palabaras aleatorias
-let palabrasRandom = ["2022", "Dogs", "Erick", "Lady", "Movie", "Series", "Anime", "Kids", "Shark", "Final", "Fantasy"];
+let palabrasRandom = ["2022", "Dogs", "Erick", "Lady", "Terror", "Series", "Ann", "Kids", "Shark", "Final", "Fantasy"];
 const random = () => {
-        let nroArray = Math.floor(Math.random() * 11);
-        console.log(palabrasRandom[nroArray]);
-    }
-    // Carga pelis cuando se completa la carga de la pagina
+    let nroArray = Math.floor(Math.random() * 11);
+    return (palabrasRandom[nroArray]);
+}
+
+
+
+// Carga pelis cuando se completa la carga de la pagina
 document.addEventListener('DOMContentLoaded', () => {
-    let url = "https://www.omdbapi.com/?apikey=669a7ccb&s=shark&r=json&page=1";
+    let url = "https://www.omdbapi.com/?apikey=669a7ccb&s=" + random() + "&r=json&page=1";
     fetch(url)
         .then(response => response.json())
         .then(datos => verInicio(datos))
@@ -59,7 +62,7 @@ const cargaMovies = (data) => {
         for (let i = 0; i < array.length; i++) {
             const element = array[i];
             console.log(element);
-            div.innerHTML += ` <div class="card-e col s2 z-depth-4">
+            div.innerHTML += ` <div class="card-e col s1 z-depth-4">
                              <img class="responsive-img" src="${element.Poster}">
                              <p class="">${element.Title}</p>
                              <a class="txt disabled right">${element.Type}</a>
@@ -82,7 +85,7 @@ const btnSearchKids = document.querySelector("#infantil");
 
 // funcion para ver por series
 btnSearchSeries.addEventListener("click", () => {
-    let url = "https://www.omdbapi.com/?apikey=669a7ccb&s=list&r=json&page=5&type=series";
+    let url = "https://www.omdbapi.com/?apikey=669a7ccb&s=" + random() + "&r=json&page=1&type=series";
     fetch(url)
         .then(response => response.json())
         .then((data) => {
@@ -103,25 +106,103 @@ btnSearchSeries.addEventListener("click", () => {
                     document.getElementById('movies').classList.add('movies');
                 }
 
-            } else {
-                div.innerHTML = `<center><h5>Ha ocurrido un error con el servidor</h5></center>`
             }
         })
-        .catch(error => console.log(error))
+        .catch(error = () => {
+            div.innerHTML = `<center><h5 class="">Ha ocurrido un error al cargar series, intente nuevamente</h5></center>`;
+        })
 
 });
 
 // funcion para ver por Movies
 btnSearchMovies.addEventListener("click", () => {
-    alert("Movies");
+    let url = "https://www.omdbapi.com/?apikey=669a7ccb&s=" + random() + "&r=json&page=1&type=movie";
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            // console.log(data);
+            div.innerHTML = "";
+            array = data.Search;
+            if (data != null) {
+                for (let i = 0; i < array.length; i++) {
+                    const element = array[i];
+                    console.log(element);
+                    div.innerHTML += ` <div class="card-e col s2 z-depth-4">
+                                     <img class="responsive-img" src="${element.Poster}">
+                                     <p class="">${element.Title}</p>
+                                     <a class="txt disabled right">${element.Type}</a>
+                                     <p class="">Fecha de lanzamiento :${element.Year}</p>
+                                       </div>`
+                        // Animacion de carga en pelis
+                    document.getElementById('movies').classList.add('movies');
+                }
+
+            }
+        })
+        .catch(error = () => {
+            div.innerHTML = `<center><h5 class="">Ha ocurrido un error al cargar peliculas, intente nuevamente</h5></center>`;
+        })
+
 })
 
 // funcion para ver por Anime
 btnSearchAnime.addEventListener("click", () => {
-    alert("Anime");
+    let url = "https://www.omdbapi.com/?apikey=669a7ccb&s=anime&r=json&page=1&type=";
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            // console.log(data);
+            div.innerHTML = "";
+            array = data.Search;
+            if (data != null) {
+                for (let i = 0; i < array.length; i++) {
+                    const element = array[i];
+                    console.log(element);
+                    div.innerHTML += ` <div class="card-e col s2 z-depth-4">
+                                     <img class="responsive-img" src="${element.Poster}">
+                                     <p class="">${element.Title}</p>
+                                     <a class="txt disabled right">${element.Type}</a>
+                                     <p class="">Fecha de lanzamiento :${element.Year}</p>
+                                       </div>`
+                        // Animacion de carga en pelis
+                    document.getElementById('movies').classList.add('movies');
+                }
+
+            }
+        })
+        .catch(error = () => {
+            div.innerHTML = `<center><h5 class="">Ha ocurrido un error al cargar Animes, intente nuevamente</h5></center>`;
+        })
+
 })
 
 // funcion para ver por Kids
 btnSearchKids.addEventListener("click", () => {
-    alert("Kids");
+    let url = "https://www.omdbapi.com/?apikey=669a7ccb&s=kids&r=json&page=1&type=series";
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            // console.log(data);
+            div.innerHTML = "";
+            array = data.Search;
+            if (data != null) {
+                for (let i = 0; i < array.length; i++) {
+                    const element = array[i];
+                    console.log(element);
+                    div.innerHTML += ` <div class="card-e col s2 z-depth-4">
+                                     <img class="responsive-img" src="${element.Poster}">
+                                     <p class="">${element.Title}</p>
+                                     <a class="txt disabled right">${element.Type}</a>
+                                     <p class="">Fecha de lanzamiento :${element.Year}</p>
+                                       </div>`
+                        // Animacion de carga en pelis
+                    document.getElementById('movies').classList.add('movies');
+                }
+
+            }
+        })
+        .catch(error = () => {
+            div.innerHTML = `<center><h5 class="">Ha ocurrido un error al cargar infantil, intente nuevamente</h5></center>`;
+        })
+
 })
