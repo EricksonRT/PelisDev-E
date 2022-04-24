d5409b4df6b54f79c59369f75c31d3a35a87d303
EricksonRT <erik_lauri@hotmail.com>
2022-04-24 14:11:20 -0300

    efecto smoothv2

--- js/main.js
+++ js/main.js
@@ -64,15 +64,9 @@ const cargaMovies = (data) => {
             console.log(element);
             div.innerHTML += ` <div class="card-e col s2 z-depth-4">
                              <img class="responsive-img" src="${element.Poster}">
-<<<<<<< HEAD
                              <p class="">${element.Title}</p>
                              <a class="txt disabled">${element.Type}</a>
                              <p class="disable">Año de lanzamiento: ${element.Year}</p>
-=======
-                             <a class="txt disabled right">${element.Type}</a>
-                             <p class="">${element.Title}<hr></p>
-                             <p class="">Fecha de lanzamiento :${element.Year}</p>
->>>>>>> 70b45c4c3c964aa85feeeee06e9bdf73d6804a9f
                                </div>`
                 // Animacion de carga en pelis
             document.getElementById('movies').classList.toggle('movies');
